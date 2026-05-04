63d9d6b961d870df27d55ef254b6dd7ab5d12fe2
Abdurehman <abdurehmanali611@gmail.com>
2026-05-04 10:31:41 +0300

    Changes

--- config/settings.py
+++ config/settings.py
@@ -24,8 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-import os
-os.environ.pop('DEBUG', None)
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
